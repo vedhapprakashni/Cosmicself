87425d3cab139d7db26102b86e11dbc28951ecda
vedhapprakashni <vedhamuthukumar@gmail.com>
2025-12-24 00:29:17 +0530

    Fix Express wildcard route for path-to-regexp compatibility

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'production') {
   const clientBuildPath = path.join(__dirname, '../client/dist');
   app.use(express.static(clientBuildPath));
 
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(clientBuildPath, 'index.html'));
   });
 }
