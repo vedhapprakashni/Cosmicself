f3a03c0954e10ebbcbe6ec7d7bf439fab091d188
vedhapprakashni <vedhamuthukumar@gmail.com>
2025-12-28 00:27:34 +0530

    edit

--- server/index.js
+++ server/index.js
@@ -37,11 +37,11 @@ if (process.env.NODE_ENV === 'production') {
   const clientBuildPath = path.join(__dirname, '../client/dist');
   app.use(express.static(clientBuildPath));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(clientBuildPath, 'index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(clientBuildPath, '../client/dist/index.html'));
   });
 }
-
+//
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
